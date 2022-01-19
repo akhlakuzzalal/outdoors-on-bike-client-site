47cf4ba0d641bcb2f9b52e809aac3cc6b75e17e7
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-01-20 02:17:51 +0600

    [MOD]review

--- src/components/Pages/Home/Reviews/Review/ReviewHome.js
+++ src/components/Pages/Home/Reviews/Review/ReviewHome.js
@@ -1,21 +1,44 @@
 import { Rating } from '@mui/material';
 import React from 'react';
+import { useState } from 'react';
 
 const ReviewHome = ({ review }) => {
-   const { name, img, rate, text } = review
+   const { name, img, rate, text } = review;
+   let [displayText, setDisplay] = useState('none')
+   let [btnText, setBtnTxt] = useState('read more')
+   const expand = () => {
+      if (displayText === 'none') {
+         setDisplay('inline')
+         setBtnTxt('read less')
+      }
+      if (displayText === 'inline') {
+         setDisplay('none')
+         setBtnTxt('read more')
+      }
+   }
    return (
-      <div class="col-lg-4">
-         <div className="card-style">
-            <div class="d-flex justify-content-center pt-5">
-               <img class="w-25 border rounded-circle card-img-top"
-                  src={img}
-                  alt="..." />
-            </div>
-            <div class="card-body px-4 text-center">
-               <p class="card-text">{text}</p>
-               <Rating name="read-only" value={rate} readOnly />
-               <p class="text-primary">{name}</p>
-            </div>
+      <div className="review-style mx-1 my-2" style={{ minHeight: '350px' }}>
+         <div class="d-flex justify-content-center pt-5">
+            <img class="w-25 border rounded-circle card-img-top"
+               src={img}
+               alt="..." />
+         </div>
+         <div class="card-body px-4 text-center">
+            <p class="card-text">{text.slice(0, 100)}
+               <span id='more' style={{ display: `${displayText}` }}>
+                  {text.slice(100, 600)}
+               </span>
+               {
+                  text?.length > 100 &&
+                  <>
+                     <button className='more-btn' onClick={() => expand()}>
+                        ...{btnText}
+                     </button>
+                  </>
+               }
+            </p>
+            <Rating name="read-only" value={rate} readOnly />
+            <p class="text-primary">{name}</p>
          </div>
       </div>
    );
--- src/components/Pages/Home/Reviews/Reviews.js
+++ src/components/Pages/Home/Reviews/Reviews.js
@@ -1,22 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import ReviewHome from './Review/ReviewHome';
+import { Carousel } from '@trendyol-js/react-carousel';
+import { Spinner } from 'react-bootstrap';
+import { Rating } from '@mui/material';
 
 const Reviews = () => {
    const [reviews, setReviews] = useState([]);
+   let ret = 0
 
    useEffect(() => {
       fetch('https://bike-website-server.herokuapp.com/reviews')
          .then(res => res.json())
          .then(data => setReviews(data))
-   }, [])
+   }, []);
+
+   if (reviews.length > 0) {
+      for (let r of reviews) {
+         ret += r.rate
+      }
+   }
+
+   console.log((ret / reviews.length).toFixed(1))
    return (
       <div className='container'>
          <h2 style={{ color: '#f57f17', padding: '80px 0' }} className='pb-2 pt-4 fs-2 fw-bold mt-4'>Users Review</h2>
-         <div className='row mb-4'>
-            {
-               reviews.map(review => <ReviewHome key={review._id} review={review}></ReviewHome>)
-            }
+         <div className='d-flex justify-content-center'>
+            <Rating name="read-only" value={(ret / reviews.length).toFixed(1)} readOnly /> <p>  ({(ret / reviews.length).toFixed(1)})</p>
          </div>
+         {reviews.length > 0 ?
+            <Carousel className='review-carosel' show={3.3} slide={1} swiping={true}>
+               {
+                  reviews.map(review => <ReviewHome key={review._id} review={review}></ReviewHome>)
+               }
+            </Carousel>
+            :
+            <>
+               <Spinner></Spinner>
+            </>
+         }
       </div>
    );
 };
